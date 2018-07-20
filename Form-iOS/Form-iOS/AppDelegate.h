4db293a1644a461452e49aff313d6d782b293e08
蔡杰 <jie.cai@mljr.com>
2018-07-20 12:29:50 +0800

    表单工程

---
+++ Form-iOS/Form-iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Form-iOS
+//
+//  Created by 蔡杰 on 2018/7/20.
+//  Copyright © 2018年 AllanCai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Form-iOS/Form-iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Form-iOS
+//
+//  Created by 蔡杰 on 2018/7/20.
+//  Copyright © 2018年 AllanCai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
