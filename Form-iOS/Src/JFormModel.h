c1be827ef0b30c5e871122989f984e833f9e21f1
蔡杰 <jie.cai@mljr.com>
2018-08-01 17:43:07 +0800

    定义 FormModel

---
+++ Form-iOS/Src/JFormModel.h
@@ -0,0 +1,55 @@
+//
+//  JFormModel.h
+//  Form-iOS
+//
+//  Created by 蔡杰 on 2018/8/1.
+//  Copyright © 2018年 AllanCai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef id (^MapFromKeyValueToSpecialValueBlock)(NSString *key,id value);
+
+@interface JFormModel : NSObject
+
+//唯一标示符
+@property (nonatomic, copy) NSString *uniqueIdentifies;
+
+
+/*
+ 
+     服务器上传
+ */
+
+@property (nonatomic, strong) id  serverKey;
+@property (nonatomic, strong) id  serverValue;
+@property (nonatomic, assign,readonly,getter=isKeysArray) BOOL keysArray;
+@property (nonatomic, strong) MapFromKeyValueToSpecialValueBlock serverKeyMap;
+
+// 强制校验 默认为yes
+@property (nonatomic, assign) BOOL required;
+
+//default NO,如果设置为YES,不上传
+@property (nonatomic, assign) BOOL ignoreKeys;
+
+@property (nonatomic, copy) NSString *errorMessage;
+
+/*
+  表格显示
+
+ */
+
+@property (nonatomic, strong) id   displayKey;
+@property (nonatomic, strong) id   display; //
+@property (nonatomic, assign)  BOOL canDisplayMerge;
+@property (nonatomic, strong) MapFromKeyValueToSpecialValueBlock disPlayKeyMap;
+
+//内部懒加载初始化
+@property (nonatomic, strong) NSDictionary *  extension;
+
+- (NSDictionary *)serverMap;
+
+- (BOOL)checkServerValueIsValidation;
+
+
+@end
---
+++ Form-iOS/Src/JFormModel.m
@@ -0,0 +1,13 @@
+//
+//  JFormModel.m
+//  Form-iOS
+//
+//  Created by 蔡杰 on 2018/8/1.
+//  Copyright © 2018年 AllanCai. All rights reserved.
+//
+
+#import "JFormModel.h"
+
+@implementation JFormModel
+
+@end
